55d48288e5a15407278d15bd0f2dde97226f5d17
Pedro <31280460+iGameKiller@users.noreply.github.com>
2020-06-03 18:50:49 -0300

    Add files via upload

---
+++ Final/Front-end-Trainees-master/Main/produtos.js
@@ -0,0 +1,24 @@
+var produto1={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - 1 Unidade", "preco":"R$ 4,50"};
+var produto2={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - kit 5 Unidades", "preco":"R$ 22,49"};
+var produto3={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - kit 10 Unidades", "preco":"R$ 44,99"};
+var produto4={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - kit 50 Unidades", "preco":"R$ 224,99"};
+
+var produto5={"tipo":"luva", "nome": "Luva de Látex Descartável - 1 Par", "preco":"R$ 0,99"};
+var produto6={"tipo":"luva", "nome": "Luva de Látex Descartável - 5 Pares", "preco":"R$ 4,49"};
+var produto7={"tipo":"luva", "nome": "Luva de Látex Descartável - 10 Pares", "preco":"R$ 8,99"};
+var produto8={"tipo":"luva", "nome": "Luva de Látex Descarpack - 50 Pares", "preco":"R$ 39,99"};
+
+var produto9={"tipo":"alcoolgel", "nome": "Álcool em Gel AsseptGel 1L - 1 Unidade", "preco":"R$ 5,49"};
+var produto10={"tipo":"alcoolgel", "nome": "Álcool em Gel PureClean Gel 1L - 5 Unidades", "preco":"R$ 58,99"};
+var produto11={"tipo":"alcoolgel", "nome": "Álcool em Gel Coperalcool 1L - 10 Unidades", "preco":"R$ 199,00"};
+var produto12={"tipo":"alcoolgel", "nome": "Álcool em Gel Urca 1L - 100 Unidades", "preco":"R$ 1190,00"};
+
+var produto13={"tipo":"alcool", "nome": "Álcool Start 70% 1L - 1 Unidade", "preco":"R$ 8,99"};
+var produto14={"tipo":"alcool", "nome": "Álcool Tupi 70% 1L - 5 Unidades", "preco":"R$ 43,99"};
+var produto15={"tipo":"alcool", "nome": "Álcool Septmax 70% 1L - 10 Unidades", "preco":"R$ 87,99"};
+var produto16={"tipo":"alcool", "nome": "Álcool Septmax 70% 1L - 100 Unidades", "preco":"R$ 890,00"};
+
+var descmasc="Máscara fabricada de acordo com as recomendações da OMS, feita em dupla camada de tecido, garantindo proteção, qualidade e conforto da respiração. Ideal para isolar vírus, poeira, germes, fumaça, poluição, etc. Possui corte ergonômico para se ajustar perfeitamente ao rosto, se adapta em qualquer formato de rosto. Produto lavável. Tecido: Tricoline e Elastano. Tipo de fixação: Elásticos que prendem atrás das orelhas (super confortável)<br> Recomendações de lavagem:<br> 1 - Lavar com água fria ou morna<br> 2 - Lavar com sabão neutro<br>"
+var descluva="Luvas descartáveis, sua qualidade praticamente elimina o risco de rompimento, minimiza o risco de contaminação por vírus e bactérias. São impermeáveis e econômicas. Possuem um toque de seda confortável	que proporciona alta sensibilidade ao tato."
+var descalcoolgel="O Álcool Gel 70º gl higienizante para as mãos da Body Blue mata 99,99% dos germes causadores de doenças, tem secagem rápida, dispensa o uso de água ou toalhas. Possui ação hidratante, rápida absorção e ação antisséptica.<br>A mão é a principal via de transmissão de micro-organismos, já que estão em constante contato com superfícies contaminadas, por isso a higienização adequada é a medida isolada individual mais eficaz para prevenir a transmissão de doenças para o ambiente e para as pessoas.<br>- Ação Hidratante<br>- Rápida absorção<br>- Ação antisséptica<br>- Mata 99,99% dos germes causadores de doença<br>- Previne a transmissão de vírus e bactérias<br>- Mãos protegidas e higienizadas<br>"
+var descalcool="Álcool Etílico Hidratado 70% INPM 5 litros<br>Nova embalagem com segurança !!!<br>Ideal para ser usado em:<br>- Proteção contra vírus e doenças;<br>- Hospitais;<br>- Clinicas;<br>- Escritórios;<br>- Cozinhas de Restaurante;<br>- Banheiros;<br>- Academias;<br>"
